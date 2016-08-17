6e2fc3e19d5b13ae6aa7ec42857793132ea445b3
root <superlucky84@gmail.com>
2016-08-17 00:19:29 -0400

    * add ssl key

--- server.js
+++ server.js
@@ -12,13 +12,32 @@ var yetify = require('yetify'),
 
 // Create an http(s) server instance to that socket.io can listen to
 if (config.server.secure) {
+	/*
     server = require('https').Server({
         key: fs.readFileSync(config.server.key),
         cert: fs.readFileSync(config.server.cert),
         passphrase: config.server.password
     }, server_handler);
+	*/
+    server = require('https').Server({
+		key: fs.readFileSync(__dirname+'/jssl.key'),
+		cert: fs.readFileSync(__dirname+'/1_superlucky.co.kr_bundle.crt')
+    }, server_handler);
+	/*
+
+  var https = require('https');
+  var httpsOption = { 
+    key: fs.readFileSync(__dirname+'/jssl.key'),
+    cert: fs.readFileSync(__dirname+'/1_superlucky.co.kr_bundle.crt')
+  }
+
+  server = https.createServer(httpsOption, app).listen(443, function(){
+    console.log("Https server listening on port " + 443);
+  });
+  */
+
 } else {
-    server = require('http').Server(server_handler);
+server = require('http').Server(server_handler);
 }
 server.listen(port);
 
